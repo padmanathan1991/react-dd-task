e656a2cfb2e49ede8c08bb708389ebefd99af0e1
padmanathan1991 <padmanathan@timeless.co>
2020-02-27 16:37:06 +0530

    Updates

---
+++ precache-manifest.d25ba36e53baa868e2277dd2e3a12a55.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06eb04fe8396ee0d14f3713c24f0bb73",
+    "url": "/react-dd-task/index.html"
+  },
+  {
+    "revision": "709b25a5822b3d8801c8",
+    "url": "/react-dd-task/static/css/2.1d3b8874.chunk.css"
+  },
+  {
+    "revision": "aad78909d9e718a04ae3",
+    "url": "/react-dd-task/static/css/main.c0d7a2cd.chunk.css"
+  },
+  {
+    "revision": "709b25a5822b3d8801c8",
+    "url": "/react-dd-task/static/js/2.a2b28d3a.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/react-dd-task/static/js/2.a2b28d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad78909d9e718a04ae3",
+    "url": "/react-dd-task/static/js/main.d4212f44.chunk.js"
+  },
+  {
+    "revision": "6a4bab474995c9cf11ed",
+    "url": "/react-dd-task/static/js/runtime-main.c784a13c.js"
+  },
+  {
+    "revision": "2a980089c300b664bd81bef48d09e987",
+    "url": "/react-dd-task/static/media/Inter-Medium.2a980089.woff2"
+  },
+  {
+    "revision": "a8ecbfc75aab9e511342b626ebac0f72",
+    "url": "/react-dd-task/static/media/Inter-Medium.a8ecbfc7.woff"
+  },
+  {
+    "revision": "f0d9a4f55eb1299a7e389ed70b40318f",
+    "url": "/react-dd-task/static/media/Inter-Medium.f0d9a4f5.eot"
+  },
+  {
+    "revision": "4d0b0acd59683d48f468ee91826e9b3a",
+    "url": "/react-dd-task/static/media/Inter-Regular.4d0b0acd.woff"
+  },
+  {
+    "revision": "74d23869c6c124f07cebd59c3e819c90",
+    "url": "/react-dd-task/static/media/Inter-Regular.74d23869.eot"
+  },
+  {
+    "revision": "93967b7503df39a6b3da86c933213cad",
+    "url": "/react-dd-task/static/media/Inter-Regular.93967b75.woff2"
+  },
+  {
+    "revision": "a9ebc2b49141eb408546abb89d6c8872",
+    "url": "/react-dd-task/static/media/Inter-SemiBold.a9ebc2b4.woff2"
+  },
+  {
+    "revision": "b3ba07286d5561df63aa433fe8454db1",
+    "url": "/react-dd-task/static/media/Inter-SemiBold.b3ba0728.woff"
+  },
+  {
+    "revision": "fed9e139ad63d8b05889aead83fd23fd",
+    "url": "/react-dd-task/static/media/Inter-SemiBold.fed9e139.eot"
+  },
+  {
+    "revision": "57ee2ba4c66ab230c9290ac5d9ec2ff0",
+    "url": "/react-dd-task/static/media/avatar.57ee2ba4.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-dd-task/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-dd-task/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-dd-task/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-dd-task/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-dd-task/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-dd-task/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "f2610513c5a854fe4600d64db50aebd7",
+    "url": "/react-dd-task/static/media/heart.f2610513.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-dd-task/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-dd-task/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-dd-task/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-dd-task/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-dd-task/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-dd-task/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-dd-task/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-dd-task/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-dd-task/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-dd-task/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
